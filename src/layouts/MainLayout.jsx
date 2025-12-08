1d98d7fe1f6cf0b95d8df464230716cdbb612ea9
AsifAhmedTanjid <asifahmedtanjid@gmail.com>
2025-12-08 10:57:17 +0600

    feat:  footer design with logo social and links and responsiveness

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "../components/shared/Navbar/Navbar";
 import { Outlet } from "react-router";
+import Footer from "../components/shared/Footer/Footer";
 
 const MainLayout = () => {
   return (
     <div>
     <Navbar></Navbar>
-    <Outlet></Outlet>
+    
+    <div className="min-h-screen"><Outlet></Outlet></div>
+    
+    <Footer></Footer>
     </div>
   );
 };
